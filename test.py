0969c3805b1e750a2eb315d17dc171fdcb5c345d
EnzeXu <xue20@wfu.edu>
2022-12-26 20:07:49 -0500

    updates

--- test.py
+++ test.py
@@ -39,7 +39,15 @@ def one_time_detail_check(model_name, time_string_list, average_length):
 if __name__ == "__main__":
     sir_origin_time_strings = ["20221226_001416","20221226_002338","20221226_003325","20221226_004312","20221226_005253","20221226_010229","20221226_011214","20221226_012134","20221226_013120","20221226_014122"]
     sir_plan3_time_strings = ["20221226_001359","20221226_002805","20221226_004222","20221226_005641","20221226_011053","20221226_012509","20221226_013916","20221226_015339","20221226_020742","20221226_022141"]
-    one_time_detail_check("SIR_Fourier_Lambda", sir_origin_time_strings, 10000)
-    one_time_detail_check("SIR_Fourier_Lambda", sir_plan3_time_strings, 10000)
+    rep_original_time_strings = ["20221226_002700","20221226_014331","20221226_030112","20221226_042011","20221226_053758","20221226_065546","20221226_081128","20221226_092939","20221226_104616","20221226_120047"]
+    rep_plan3_time_strings = ["20221226_002656","20221226_021123","20221226_035405","20221226_053835","20221226_072055","20221226_090353","20221226_104703","20221226_123144","20221226_141414","20221226_155656"]
+    print("original:")
+    one_time_detail_check("REP_Fourier_Lambda", rep_original_time_strings, 10000)
+    print("plan3:")
+    one_time_detail_check("REP_Fourier_Lambda", rep_plan3_time_strings, 10000)
+    # print("original:")
+    # one_time_detail_check("SIR_Fourier_Lambda", sir_origin_time_strings, 10000)
+    # print("plan3:")
+    # one_time_detail_check("SIR_Fourier_Lambda", sir_plan3_time_strings, 10000)
     # one_time_build_from_record()
     pass
