1aafdff12f5d84522c00273ed74d24a58029f7a9
EnzeXu <xue20@wfu.edu>
2022-12-29 19:21:01 -0500

    updates

---
+++ submit_v1229.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#sbatch jobs/lambda_turing_final_a\=original_p\=0_0-10.slurm
+#sbatch jobs/lambda_turing_final_a\=plan3_p\=0_0-10.slurm
+#
+sbatch jobs/lambda_cc1_final_a\=original_p\=0_0-10.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=0_10-20.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=0_20-30.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=0_30-40.slurm
+
+sbatch jobs/lambda_cc1_final_a\=original_p\=1_0-10.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=1_10-20.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=1_20-30.slurm
+sbatch jobs/lambda_cc1_final_a\=original_p\=1_30-40.slurm
+
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=0_0-10.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=0_10-20.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=0_20-30.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=0_30-40.slurm
+
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=1_0-10.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=1_10-20.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=1_20-30.slurm
+sbatch jobs/lambda_cc1_final_a\=plan3_p\=1_30-40.slurm
+
+sbatch jobs/lambda_cc1_final_pinn_0-10.slurm
+sbatch jobs/lambda_cc1_final_pinn_10-20.slurm
+sbatch jobs/lambda_cc1_final_pinn_20-30.slurm
+sbatch jobs/lambda_cc1_final_pinn_30-40.slurm
+#
+#sbatch jobs/lambda_rep_final_a\=original_p\=0_0-10.slurm
+#sbatch jobs/lambda_rep_final_a\=original_p\=1_0-10.slurm
+#sbatch jobs/lambda_rep_final_a\=plan3_p\=0_0-10.slurm
+#sbatch jobs/lambda_rep_final_a\=plan3_p\=1_0-10.slurm
+
+#sbatch jobs/lambda_sir_final_pinn_0-10.slurm
+#sbatch jobs/lambda_sir_final_a\=original_p\=0_0-10.slurm
+#sbatch jobs/lambda_sir_final_a\=plan3_p\=0_0-10.slurm
