204fd6d78410f5b0cec6740165c19289ca3a21dd
EnzeXu <xue20@wfu.edu>
2023-01-18 20:07:13 -0500

    updates

--- model_Toggle_Lambda.py
+++ model_Toggle_Lambda.py
@@ -21,7 +21,7 @@ class Parameters:
 
 
 class TrainArgs:
-    iteration = 10000  # 20000 -> 50000
+    iteration = 2000  # 20000 -> 50000
     epoch_step = 100  # 1000
     test_step = epoch_step * 10
     initial_lr = 0.01
