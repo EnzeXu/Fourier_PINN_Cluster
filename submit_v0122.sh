f13ed7faa8a491bed2914f13125096ebb6097666
EnzeXu <xue20@wfu.edu>
2023-01-22 17:23:41 -0500

    updates omega

--- submit_v0122.sh
+++ submit_v0122.sh
@@ -4,6 +4,6 @@ for model_name in "rep3" "rep6"
 do
     for activation in "gelu" "relu" "elu" "tanh" "sin" "softplus"
     do
-        sbatch jobs/o_${model_name}_a\=${activation}_0-10.slurm
+        sbatch jobs/o_${model_name}_${activation}_0-10.slurm
     done
 done
