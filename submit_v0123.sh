ae3b4a844b522f520421150f2023dddf3f4aeac3
EnzeXu <xue20@wfu.edu>
2023-01-23 18:31:01 -0500

    updates

--- submit_v0123.sh
+++ submit_v0123.sh
@@ -3,7 +3,7 @@
 for model_name in "sir" "siraged"
 do
     sbatch jobs/o_${model_name}_pinn_0-10.slurm
-    for activation in "gelu" "relu" "elu" "tanh" "sin" "softplus"
+    for activation in "gelu" "relu" "elu" "tanh" "sin" "softplus" "adaptive"
     do
         sbatch jobs/o_${model_name}_${activation}_0-10.slurm
     done
