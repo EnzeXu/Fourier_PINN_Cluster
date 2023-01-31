bd796a26024d37d4ec1d7f9951bd2f3cde53402a
EnzeXu <xue20@wfu.edu>
2023-01-31 16:47:35 -0500

    updates

--- submit_v0131.sh
+++ submit_v0131.sh
@@ -4,7 +4,7 @@ for model_name in "rep3" "rep6" "siraged" "sir"
 do
     for activation in "adaptive_5_0.001" "adaptive_5_0.003" "adaptive_5_0.005" "adaptive_5_0.01"
     do
-        sbatch jobs/o_${model_name}_${activation}_3-10.slurm
+        sbatch jobs/o_${model_name}_${activation}_0-5.slurm
     done
 done
 
