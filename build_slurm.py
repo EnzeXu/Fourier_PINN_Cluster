a2dc133ee74e6b7e315f2ee30d901fe5a718be7c
EnzeXu <xue20@wfu.edu>
2023-02-28 14:41:31 -0500

    updates

--- build_slurm.py
+++ build_slurm.py
@@ -610,26 +610,27 @@ def one_time_build_siraged_omega_activations():
 
 def one_time_build_omega(module_name_short, start_seed, end_seed):
     lr = 0.001
+    scheduler = "fixed"
     plans = [
         # pinn / activation / cyclic / stable / derivative / boundary /init_lr / init_weights / init_weights_strategy / scheduler
         # [1, "gelu", 0, 0, 0, 0, lr],
-        [0, "gelu", 0, 0, 0, 0, lr, "decade"],
-        [0, "relu", 0, 0, 0, 0, lr, "decade"],
-        [0, "elu", 0, 0, 0, 0, lr, "decade"],
-        [0, "tanh", 0, 0, 0, 0, lr, "decade"],
-        [0, "sin", 0, 0, 0, 0, lr, "decade"],
-        [0, "softplus", 0, 0, 0, 0, lr, "decade"],
+        [0, "gelu", 0, 0, 0, 0, lr, scheduler],
+        [0, "relu", 0, 0, 0, 0, lr, scheduler],
+        [0, "elu", 0, 0, 0, 0, lr, scheduler],
+        [0, "tanh", 0, 0, 0, 0, lr, scheduler],
+        [0, "sin", 0, 0, 0, 0, lr, scheduler],
+        [0, "softplus", 0, 0, 0, 0, lr, scheduler],
 
-        [0, "adaptive_6", 0, 0, 0, 0, 0.001, "decade", "avg", "trainable"],
-        [0, "adaptive_6", 0, 0, 0, 0, 0.003, "decade", "avg", "trainable"],
-        [0, "adaptive_6", 0, 0, 0, 0, 0.005, "decade", "avg", "trainable"],
-        [0, "adaptive_6", 0, 0, 0, 0, 0.01, "decade", "avg", "trainable"],
+        [0, "adaptive_6", 0, 0, 0, 0, 0.001, scheduler, "avg", "trainable"],
+        [0, "adaptive_6", 0, 0, 0, 0, 0.003, scheduler, "avg", "trainable"],
+        [0, "adaptive_6", 0, 0, 0, 0, 0.005, scheduler, "avg", "trainable"],
+        [0, "adaptive_6", 0, 0, 0, 0, 0.01, scheduler, "avg", "trainable"],
 
 
-        [0, "adaptive_5", 0, 0, 0, 0, 0.001, "decade", "avg", "trainable"],
-        [0, "adaptive_5", 0, 0, 0, 0, 0.003, "decade", "avg", "trainable"],
-        [0, "adaptive_5", 0, 0, 0, 0, 0.005, "decade", "avg", "trainable"],
-        [0, "adaptive_5", 0, 0, 0, 0, 0.01, "decade", "avg", "trainable"],
+        [0, "adaptive_5", 0, 0, 0, 0, 0.001, scheduler, "avg", "trainable"],
+        [0, "adaptive_5", 0, 0, 0, 0, 0.003, scheduler, "avg", "trainable"],
+        [0, "adaptive_5", 0, 0, 0, 0, 0.005, scheduler, "avg", "trainable"],
+        [0, "adaptive_5", 0, 0, 0, 0, 0.01, scheduler, "avg", "trainable"],
 
 
         # [0, "gelu", 0, 1, 0, 0, lr],
